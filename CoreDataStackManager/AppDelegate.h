3706ebf9ec274d56847b2f2490535f54fb1465c8
Axel Péju <pejuaxel@me.com>
2012-03-17 14:53:32 +0100

    Initial Commit

---
+++ CoreDataStackManager/AppDelegate.h
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.h
+//  CoreDataStackManager
+//
+//  Created by Axel Péju on 17/03/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+
+- (IBAction)saveAction:(id)sender;
+
+@end
---
+++ CoreDataStackManager/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  CoreDataStackManager
+//
+//  Created by Axel Péju on 17/03/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
